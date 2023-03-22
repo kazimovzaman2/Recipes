beee62eed3a827df478b66ac8512a21307747222
Zaman Kazimov <92586852+kazimovzaman2@users.noreply.github.com>
2023-03-22 08:02:34 +0400

    Update settings.py

--- core/settings.py
+++ core/settings.py
@@ -125,17 +125,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "/static/"
-MEDIA_URL = '/images/'
+MEDIA_URL = "static/images/"
 
-STATICFILES_DIRS = [
-    BASE_DIR / 'static'
-]
-
-MEDIA_ROOT = os.path.join(BASE_DIR, 'static/images')
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+MEDIA_ROOT = os.path.join(BASE_DIR, "images")
 
 # Deployment
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 
